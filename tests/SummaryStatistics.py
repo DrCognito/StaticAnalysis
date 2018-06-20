7888e23b92305a93487144634504a2dba541e85b
M Chadwick <dr.m.chadwick@gmail.com>
2018-06-21 00:57:42 +0100

    Test new cumulative player stats.

--- tests/SummaryStatistics.py
+++ tests/SummaryStatistics.py
@@ -3,8 +3,11 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from analysis.Replay import win_rate_table
+from analysis.Player import cumulative_player
 from replays.Replay import Replay, determine_side_byteam
+from replays.Player import Player
 from replays.TeamSelections import TeamSelections
+from lib.team_info import Teams
 
 s_maker = Setup.get_testDB()
 session = s_maker()
@@ -14,5 +17,10 @@ team_test_id = 5229049
 
 filt = (Replay.teams.any(TeamSelections.teamID == team_test_id), )
 
-test = win_rate_table(session, filt,
-                      lambda x: determine_side_byteam(team_test_id, x))
+test = win_rate_table(session, Teams['Mad Lads'])
+print(test)
+
+#Synderen
+p_filter = (Player.steamID == 76561197964547457)
+test_p = cumulative_player(session, 'kills', Teams['Mad Lads'], p_filter)
+print(test_p.resample('10T').sum())
