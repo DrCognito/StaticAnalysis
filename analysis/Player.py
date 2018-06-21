5aa75e1aeb334fe80c90aede635bf05bd8328770
M Chadwick <dr.m.chadwick@gmail.com>
2018-06-21 17:14:37 +0100

    Initial work on player picks, not accumulating properly atm.

--- analysis/Player.py
+++ analysis/Player.py
@@ -1,11 +1,12 @@
 from .Statistics import x_vs_time, xy_vs_time
 from datetime import timedelta
-from pandas import Series
+from pandas import Series, concat
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from replays.Player import Player
 from replays.Replay import Replay
+from sqlalchemy import and_
 
 
 def cumulative_player(session, prop_name, team, filt):
@@ -14,6 +15,7 @@ def cumulative_player(session, prop_name, team, filt):
                                    .join(Replay).filter(team.filter)
     # print(players)
     output = Series()
+    count = team.replay_count(session)
 
     for p in players:
         off_set_time = p.replay.gameStart
@@ -24,4 +26,39 @@ def cumulative_player(session, prop_name, team, filt):
             else:
                 output[time] = prop.increment
 
+    if count > 0:
+        output = output / count
+
     return output
+
+
+def player_heroes(session, team, r_filt=None, p_filt=None):
+    player_series = []
+
+    if r_filt is None:
+            r_filt = team.filter
+    else:
+        r_filt = and_(team.filter, r_filt)
+
+    for player in team.player_ids:
+
+        pid = team.player_ids[player]
+
+        if p_filt is None:
+            p_f = Player.steamID == pid
+        else:
+            p_f = and_(Player.steamID == pid, p_filt)
+
+        p_picks = session.query(Player.hero).filter(p_f)\
+                                            .join(Replay).filter(r_filt)
+
+        p_res = Series(name=player)
+        for pick in p_picks:
+            if pick in p_res:
+                p_res[pick] += 1
+            else:
+                p_res[pick] = 1
+
+        player_series.append(p_res)
+
+    return concat(player_series, axis=1).fillna(0)
--- tests/SummaryStatistics.py
+++ tests/SummaryStatistics.py
@@ -3,7 +3,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from analysis.Replay import win_rate_table
-from analysis.Player import cumulative_player
+from analysis.Player import cumulative_player, player_heroes
 from replays.Replay import Replay, determine_side_byteam
 from replays.Player import Player
 from replays.TeamSelections import TeamSelections
@@ -24,3 +24,6 @@ print(test)
 p_filter = (Player.steamID == 76561198047004422)
 test_p = cumulative_player(session, 'last_hits', Teams['Mad Lads'], p_filter)
 print(test_p.resample('10T').sum())
+
+test_picks = player_heroes(session, Teams['Mad Lads'])
+print(test_picks)
