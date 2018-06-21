d294522452c75d9835c326eaca3edd59e613d3f9
M Chadwick <dr.m.chadwick@gmail.com>
2018-06-22 00:02:07 +0100

    Add summarise variable. Fix counting by unpacking row properly.

--- analysis/Player.py
+++ analysis/Player.py
@@ -32,7 +32,7 @@ def cumulative_player(session, prop_name, team, filt):
     return output
 
 
-def player_heroes(session, team, r_filt=None, p_filt=None):
+def player_heroes(session, team, summarise=10, r_filt=None, p_filt=None):
     player_series = []
 
     if r_filt is None:
@@ -54,10 +54,17 @@ def player_heroes(session, team, r_filt=None, p_filt=None):
 
         p_res = Series(name=player)
         for pick in p_picks:
-            if pick in p_res:
-                p_res[pick] += 1
+            if pick[0] in p_res:
+                p_res[pick[0]] += 1
             else:
-                p_res[pick] = 1
+                p_res[pick[0]] = 1
+
+        if summarise and len(p_res) > summarise:
+            p_res.sort_values(ascending=False, inplace=True)
+            other = p_res[summarise:].sum()
+            p_res['Other'] = other
+            p_res.sort_values(ascending=False, inplace=True)
+            p_res = p_res[:summarise + 1]
 
         player_series.append(p_res)
 
