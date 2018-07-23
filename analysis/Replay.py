63c173203758b19eeabcdb98b14136348ef1ce59
M Chadwick <dr.m.chadwick@gmail.com>
2018-07-23 17:40:39 +0100

    Working hero_win_rate. Added function for retrieving a Type for a team in range of time.

--- analysis/Replay.py
+++ analysis/Replay.py
@@ -3,7 +3,11 @@ from pandas import DataFrame
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from replays.PositionTimeBase import PositionTimeBase
 from replays.Replay import Replay, Team
+from replays.Ward import Ward, WardType
+from lib.team_info import TeamInfo
+from sqlalchemy import and_
 
 
 def win_rate_table(session, team):
@@ -53,11 +57,46 @@ def hero_win_rate(r_query, team):
                     output.loc[hero, column] += 1
                 else:
                     output.loc[hero, column] = 1
+                    other_col = 'Loss' if is_win else 'Win'
+                    output.loc[hero, other_col] = 0
 
     _process(Team.DIRE)
     _process(Team.RADIANT)
+
     output.fillna(0, inplace=True)
-    output['Total'] = output['Win'] + ['Loss']
+    output['Total'] = output['Win'] + output['Loss']
     output['Rate'] = output['Win']/output['Total']
 
     return output
+
+
+def get_ptbase_tslice(session, r_query, Type,
+                      team: TeamInfo, start=None, end=None):
+    if start is not None and end is not None:
+        assert(end >= start)
+        t_filter = and_(Type.game_time > start, Type.game_time <= end)
+    elif start is not None:
+        t_filter = Type.game_time > start
+    elif end is not None:
+        t_filter = Type.game_time <= end
+    else:
+        t_filter = None
+
+    def _process_side(side):
+        r_filter = Replay.get_side_filter(team, side)
+        replays = r_query.filter(r_filter)
+
+        if t_filter is not None:
+            w_query = session.query(Type).filter(t_filter)\
+                                         .filter(Type.team == side)\
+                                         .join(replays)
+        else:
+            w_query = session.query(Type).filter(Type.team == side)\
+                                         .join(replays)
+
+        return w_query
+
+    return _process_side(Team.DIRE), _process_side(Team.RADIANT)
+
+
+
