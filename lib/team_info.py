7ff4b1bac2ee69e7ad53eca901306e1760a07a12
M Chadwick <dr.m.chadwick@gmail.com>
2018-07-25 00:55:56 +0100

    Converted to database based team info. TeamInfo is now a wrapper for the format that must be consistent with Team_Info.db.

--- lib/team_info.py
+++ lib/team_info.py
@@ -6,45 +6,58 @@ from replays.Replay import Replay
 from replays.TeamSelections import TeamSelections
 from sqlalchemy import or_
 
+from datetime import datetime, timedelta
+from os import environ as environment
 
-class TeamInfo():
-    def __init__(self, name, players, team_id=None, filt=None):
-        '''name: Team name.
-           players: OrderedDict of player names: steam_ids.
-           team_id: Numerical team_id, will be used to get replays
-           if filter undefined.
-           filt: SqlAlchemy filter to identify team replays.
-           One of filt or team_id must be defined.
-        '''
-        self.name = name
-        self.player_ids = players
-        self._filter = filt
-        self.team_id = team_id
-        self._stack_id = None
+from sqlalchemy import (BigInteger, Column, DateTime, ForeignKey, Integer,
+                        String, create_engine)
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import relationship
 
-        if self.team_id is None and self._filter is None:
-            raise ValueError("TeamInfo class must be defined with at least a filter or team_id.")
 
-    @property
-    def stack_id(self):
-        if self._stack_id is None:
-            ids = list()
-            for name in self.player_ids:
-                ids.append(self.player_ids[name])
+Base_TI = declarative_base()
 
-            ids.sort()
-            self._stack_id = ''.join(str(i) for i in ids)
 
-        return self._stack_id
+class TeamInfo_DB(Base_TI):
+    __tablename__ = "team_info"
+    team_id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+    last_change = Column(DateTime)
+    stack_id = Column(String)
+
+    players = relationship("TeamPlayer")
+
+
+class TeamPlayer(Base_TI):
+    __tablename__ = "team_players"
+    player_id = Column(BigInteger, primary_key=True)
+    team_id = Column(Integer, ForeignKey(TeamInfo_DB.team_id),
+                     primary_key=True)
+    name = Column(String)
+
+
+def InitTeamDB(path=None):
+    if path is None:
+        path = environment["TEAM_DB_PATH"]
+    engine = create_engine(path, echo=False)
+    Base_TI.metadata.create_all(engine)
+
+    return engine
+
+
+# Adapt team_db class
+class TeamInfo(TeamInfo_DB):
+    def __init__(self):
+        print("constructor!")
+        self._filter = None
 
     @property
     def filter(self):
-        if self._filter is None:
-            id_filter = Replay.teams.any(TeamSelections.teamID == self.team_id)
-            stack_filter = Replay.teams.any(TeamSelections.stackID == self.stack_id)
-            self._filter = or_(id_filter, stack_filter)
-
-        return self._filter
+        id_filter = Replay.teams.any(TeamSelections.teamID == self.team_id)
+        stack_filter = Replay.teams.any(TeamSelections.stackID == self.stack_id)
+        return or_(id_filter, stack_filter)
 
     def replay_count(self, session):
         return session.query(Replay).filter(self.filter).count()
@@ -57,16 +70,17 @@ class TeamInfo():
                                         .filter(additional_filter)
 
 
-Teams = {}
-TeamIDs = {}
-PlayerIDs = {}
+# Teams = {}
+# InitTeamDB()
+# TeamIDs = {}
+# PlayerIDs = {}
 
-PlayerIDs['Mad Lads'] = OrderedDict()
-PlayerIDs['Mad Lads']['Qojkva'] = 76561198047004422
-PlayerIDs['Mad Lads']['Madara'] = 76561198055695796
-PlayerIDs['Mad Lads']['Khezu'] = 76561198129291346
-PlayerIDs['Mad Lads']['MaybeNextTime'] = 76561198047219142
-PlayerIDs['Mad Lads']['Synderen'] = 76561197964547457
-TeamIDs['Mad Lads'] = 5229049
-Teams['Mad Lads'] = TeamInfo(name='Mad Lads', players=PlayerIDs['Mad Lads'],
-                             team_id=TeamIDs['Mad Lads'])
+# PlayerIDs['Mad Lads'] = OrderedDict()
+# PlayerIDs['Mad Lads']['Qojkva'] = 76561198047004422
+# PlayerIDs['Mad Lads']['Madara'] = 76561198055695796
+# PlayerIDs['Mad Lads']['Khezu'] = 76561198129291346
+# PlayerIDs['Mad Lads']['MaybeNextTime'] = 76561198047219142
+# PlayerIDs['Mad Lads']['Synderen'] = 76561197964547457
+# TeamIDs['Mad Lads'] = 5229049
+# Teams['Mad Lads'] = TeamInfo(name='Mad Lads', players=PlayerIDs['Mad Lads'],
+#                              team_id=TeamIDs['Mad Lads'])
