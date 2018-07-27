db10915ac70ef68e507af999234447731130e59a
M Chadwick <dr.m.chadwick@gmail.com>
2018-07-27 18:01:03 +0100

    Implement functions to replace text labels with icons as old process. x_loc has to be retrieved from x_axis.get_major_locator()

--- analysis/visualisation.py
+++ analysis/visualisation.py
@@ -1,5 +1,14 @@
+import matplotlib.pyplot as plt
 import numpy as np
-from pandas import DataFrame, read_sql
+from matplotlib.axes import Axes
+from matplotlib.offsetbox import AnnotationBbox, OffsetImage
+from pandas import DataFrame, Series, read_sql
+
+from lib.HeroTools import HeroIconPrefix, HeroIDType, convertName
+from PIL import Image
+
+colourList = ['cool', 'summer', 'winter', 'spring', 'copper']
+
 
 def plot_map_points(query, bins=128):
     coordinates = ((q.xCoordinate, q.yCoordinate) for q in query)
@@ -18,3 +27,82 @@ def plot_hexbin_time(query, Type, session, bin_size=128):
     data = read_sql(sql_query, session.bind)
 
     return data
+
+
+def make_image_annotation(icon, axes, x, y, size=1.0):
+    icon = Image.open(icon)
+    # Resize if necessary
+    if size != 1.0:
+        width, height = icon.size
+        width = width*size
+        height = height*size
+        icon.thumbnail((width, height))
+
+    imagebox = OffsetImage(icon)
+    imagebox.image.axes = axes
+
+    ab = AnnotationBbox(imagebox, (x, y),
+                        xycoords='axes fraction',
+                        boxcoords="offset points",
+                        pad=0,
+                        frameon=False
+                        )
+
+    axes.add_artist(ab)
+
+    return imagebox
+
+
+def x_label_icon(axis, y_pos=-0.15, size=1.0):
+    x_axis = axis.get_xaxis()
+
+    x_labels = x_axis.get_majorticklabels()
+    x_locations = x_axis.get_major_locator()
+
+    x_min, x_max = axis.get_xlim()
+    x_range = x_max - x_min
+
+    extra_artists = []
+    for label, x_loc in zip(x_labels, x_locations.locs):
+        # This position wont work for some plots
+        # x, _ = label.get_position()
+        x = x_loc
+        hero = label.get_text()
+        try:
+            # Get and resize the hero icon.
+            icon = HeroIconPrefix / convertName(hero, HeroIDType.NPC_NAME,
+                                                HeroIDType.ICON_FILENAME)
+        except ValueError:
+            print("Unable to find hero icon for: " + hero)
+            continue
+        x_rel = (float(x) - x_min)/x_range
+        artist = make_image_annotation(icon, axis, x_rel, y_pos, size)
+        extra_artists.append(artist)
+
+    # Remove the old xlabels
+    axis.set_xticklabels([])
+    return extra_artists
+
+
+def plot_player_heroes(data: DataFrame):
+
+    figure, axes = plt.subplots(5)
+    figure.set_size_inches(6, 10)
+
+    def _plot_player(column: Series, name: str, axis, colour: str):
+        # This filters out zeroes in a series
+        column = column.iloc[column.nonzero()]
+        column.sort_values(ascending=False, inplace=True)
+        ax: Axes = column.plot.bar(ax=axis, sharey=True, colormap=colour)
+        ax.set_ylabel(name)
+        extra_artists = x_label_icon(ax, y_pos=-0.2, size=0.8)
+
+        return ax, extra_artists
+
+    extra_artists = []
+    for i_ax, player in enumerate(data.columns):
+        ax, extra = _plot_player(data[player], player, axes[i_ax], colour=colourList[i_ax])
+        extra_artists += extra
+
+    return extra_artists
+
