0b9863202d9d2255d6c42cd28e5e3df73976e5a6
M Chadwick <dr.m.chadwick@gmail.com>
2018-07-31 15:53:52 +0100

    Added fountain and ancient coordinates.

--- lib/Common.py
+++ lib/Common.py
@@ -17,3 +17,9 @@ def average_coorinates(coordinates):
     if entries == 0:
         return None
     return (tot_x/entries, tot_y/entries)
+
+
+radiant_fountain_cords = [[22/1024, 60/1024], [117/1024, 157/1024]]
+radiant_ancient_cords = [[22/1024, 60/1024], [293/1024, 322/1024]]
+dire_fountain_cords = [[901/1024, 879/1024], [993/1024, 975/1024]]
+dire_ancient_cords = [[714/1024, 666/1024], [993/1024, 975/1024]]
