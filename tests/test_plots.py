aa0cfccc08b0371a1a8471fcc070be5811ab32df
M Chadwick <dr.m.chadwick@gmail.com>
2018-08-02 01:49:35 +0100

    Added testing for all new plots in visualisation (commented by default).

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -7,7 +7,9 @@ from analysis.visualisation import (dataframe_xy, dataframe_xy_time,
                                     plot_player_positioning,
                                     plot_object_position,
                                     dataframe_xy_time_smoke,
-                                    plot_object_position_scatter)
+                                    plot_object_position_scatter,
+                                    plot_draft_summary,
+                                    plot_pick_pairs)
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -28,27 +30,31 @@ from os import environ as environment
 #             bbox_inches='tight')
 
 p5_pos = sstat.test_player_position()
-# test_plot(p5_pos[0])
-# test_plot(p5_pos[1])
 
 dire_ancient_filter = location_filter(dire_ancient_cords, PlayerStatus)
 radiant_ancient_filter = location_filter(radiant_ancient_cords, PlayerStatus)
-plot_player_positioning(dataframe_xy(p5_pos[0], PlayerStatus, sstat.session))
+# plot_player_positioning(dataframe_xy(p5_pos[0], PlayerStatus, sstat.session))
 
 p5_dire_filt = p5_pos[0].filter(dire_ancient_filter)
 p5_radiant_filt = p5_pos[1].filter(radiant_ancient_filter)
 
-plot_player_positioning(dataframe_xy(p5_dire_filt, PlayerStatus, sstat.session))
+# plot_player_positioning(dataframe_xy(p5_dire_filt, PlayerStatus, sstat.session))
 
 wards = sstat.test_wards()
 ward_df = dataframe_xy_time(wards[0], Ward, sstat.session)
-plot_object_position(ward_df)
+# plot_object_position(ward_df)
 
 scans = sstat.test_scans()
 scans_df = dataframe_xy_time(scans[0], Scan, sstat.session)
-plot_object_position_scatter(scans_df)
-plot_object_position(scans_df)
+# plot_object_position_scatter(scans_df)
+# plot_object_position(scans_df)
 
 smokes = sstat.test_smokes()
 smokes_df = dataframe_xy_time_smoke(smokes[0], Smoke, sstat.session)
-plot_object_position(smokes_df, bins=16)
+# plot_object_position(smokes_df, bins=16)
+
+draft_summary = sstat.test_draft_summary()
+plot_draft_summary(draft_summary[0], draft_summary[1])
+
+pairs = sstat.test_pairs()
+_, ea_pairs = plot_pick_pairs(pairs)
