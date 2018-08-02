39da3e9c431091b32820d95651e2b755de0d9749
M Chadwick <dr.m.chadwick@gmail.com>
2018-08-02 17:58:48 +0100

    Initial commit of metadata skeleton.

---
+++ lib/metadata.py
@@ -0,0 +1,43 @@
+def make_meta(dataset="default"):
+    meta = {
+        'name': dataset,
+        'time_cut': None,
+        'leagues': None,
+        'replays': None,
+
+        'plot_dire_drafts': None,
+        'plot_radiant_drafts': None,
+
+        'plot_p1pos_dire': None,
+        'plot_p2pos_dire': None,
+        'plot_p3pos_dire': None,
+        'plot_p4pos_dire': None,
+        'plot_p5pos_dire': None,
+
+        'plot_p1pos_radiant': None,
+        'plot_p2pos_radiant': None,
+        'plot_p3pos_radiant': None,
+        'plot_p4pos_radiant': None,
+        'plot_p5pos_radiant': None,
+
+        'plot_ward_t1_dire': None,
+        'plot_ward_t2_dire': None,
+        'plot_ward_t3_dire': None,
+
+        'plot_ward_t1_radiant': None,
+        'plot_ward_t2_radiant': None,
+        'plot_ward_t3_radiant': None,
+
+        'plot_scan_dire': None,
+        'plot_scan_radiant': None,
+
+        'plot_smoke_dire': None,
+        'plot_smoke_radiant': None,
+
+        'plot_draft_summary': None,
+        'plot_pair_picks': None,
+        'plot_pick_context': None,
+        'plot_win_rate': None
+    }
+
+    return meta
