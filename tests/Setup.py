ac532914019392eacf487a749ba6521762c69078
M Chadwick <dr.m.chadwick@gmail.com>
2018-08-10 01:59:55 +0100

    Added support for accessing the full DB instead of testDB.

--- tests/Setup.py
+++ tests/Setup.py
@@ -1,15 +1,26 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from os import environ
 
 from replays.Replay import populate_from_JSON_file, InitDB
 from pathlib import Path
 from sqlalchemy.orm import sessionmaker
 from lib.team_info import InitTeamDB
+from dotenv import load_dotenv
 
 engine = InitDB('sqlite:///Data/testing.db')
 Session = sessionmaker(bind=engine)
 session = Session()
+
+load_dotenv(dotenv_path="setup.env")
+DB_PATH = environ['PARSED_DB_PATH']
+engine_full = InitDB(DB_PATH)
+Session_full = sessionmaker(bind=engine_full)
+
+
+def get_fullDB():
+    return Session_full()
 
 
 def make_testDB(skip_existing=True):
