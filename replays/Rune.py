bc3c7f7d80f902985429ec480ed9d1259b85b54b
M Chadwick <dr.m.chadwick@gmail.com>
2018-08-10 19:11:11 +0100

    Extended game_time to allow for in python retrieval.

--- replays/Rune.py
+++ replays/Rune.py
@@ -42,6 +42,10 @@ class Rune(Base):
 
     @hybrid_property
     def game_time(self):
+        return self.time - self.replay.creepSpawn
+
+    @game_time.expression
+    def game_time(self):
         from .Replay import Replay
         creepSpawn = select([Replay.creepSpawn]).\
             where(self.replayID == Replay.replayID).as_scalar()
