3dbe5c4692b8e54c3c0fd25255140fb0ab092536
Matt Chadwick <dr.m.chadwick@gmail.com>
2020-07-21 01:40:49 +0100

    Retrieve subquery first to fix new alchemy behaviour.

--- analysis/Player.py
+++ analysis/Player.py
@@ -131,14 +131,14 @@ def player_position(session, r_query, team: TeamInfo, player_slot: int,
         steam_id = team.players[player_slot].player_id
 
         r_filter = Replay.get_side_filter(team, side)
-        replays = r_query.filter(r_filter)
+        replays = r_query.filter(r_filter).subquery()
         
         p_filter = t_filter + (PlayerStatus.steamID == steam_id,)
 
         player_q = session.query(PlayerStatus)\
                           .filter(*p_filter)\
                           .join(replays)
-        
+
         return player_q
 
     return _process_side(Team.DIRE), _process_side(Team.RADIANT)
