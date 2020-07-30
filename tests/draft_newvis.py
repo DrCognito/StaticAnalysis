8830c258a47125e35e71757a82f70bc0625dde84
Matt Chadwick <dr.m.chadwick@gmail.com>
2020-07-30 01:33:40 +0100

    Testing new drafting

--- tests/draft_newvis.py
+++ tests/draft_newvis.py
@@ -1,12 +1,18 @@
-import Setup
 import os
 import sys
 from os import environ as environment
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from dotenv import load_dotenv
-from analysis.draft_vis import hero_box_image, process_team_portrait, hero_box_image_portrait, replay_draft_image, process_team_portrait_dotabuff
-from replays.Replay import Replay, Team
+
+import Setup
+from analysis.draft_vis import (hero_box_image, hero_box_image_portrait,
+                                process_team_dotabuff, process_team_portrait,
+                                process_team_portrait_dotabuff,
+                                replay_draft_image, pickban_line_image)
 from lib.team_info import InitTeamDB, TeamInfo, TeamPlayer
+from replays.Replay import Replay, Team
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # DB Setups
 session = Setup.get_fullDB()
@@ -30,10 +36,16 @@ for t in replay.teams:
     if t.team == Team.DIRE:
         radiant_old = process_team_portrait(replay, t)
         radiant = process_team_portrait_dotabuff(replay, t)
+        radiant2 = process_team_dotabuff(replay, t)
     else:
         dire = process_team_portrait_dotabuff(replay, t)
+        dire2 = process_team_dotabuff(replay, t)
 
-radiant.show()
+full_line = pickban_line_image(replay, Team.DIRE)
+
+radiant2.show()
+dire2.show()
+full_line.show()
 # radiant.save('./rtest.png')
 # dire.save('./dtest.png')
 
