f4ce7b1b3573b51b464ba1ee9eed7781f88ec553
Matthew Chadwick <dr.m.chadwick@gmail.com>
2021-12-03 00:43:37 +0000

    Scrims file updating from SCRIMS_PATH

---
+++ update_scrims.py
@@ -0,0 +1,13 @@
+import pathlib
+from dotenv import load_dotenv
+from os import environ as environment
+
+load_dotenv(dotenv_path="setup.env")
+SCRIMS_PATH = environment['SCRIMS_DIR']
+
+scrims_dir = pathlib.Path(SCRIMS_PATH)
+scrims = list(scrims_dir.glob('*.dem'))
+
+with open('scrims.txt', 'a') as f:
+    for s in scrims:
+        f.write(f"{s.stem}\n")
