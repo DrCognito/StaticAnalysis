4b840748e4c69804e40f34e8adebcc6054adcca6
Matthew Chadwick <dr.m.chadwick@gmail.com>
2022-05-08 20:50:30 +0100

    Added function to handle new team name map. Added function to convert a team name to a known id or fail nicely. Changed the processing to output a json file that lists scrims by team. Output list of unknown teams at the end.

--- update_scrims.py
+++ update_scrims.py
@@ -1,26 +1,38 @@
+from ast import Str
 import pathlib
 from dotenv import load_dotenv
 from os import environ as environment
+from lib.team_info import InitTeamDB, TeamInfo
+from sqlalchemy.orm import sessionmaker
 import pygsheets
 import json
 
 load_dotenv(dotenv_path="setup.env")
-SCRIMS_PATH = environment['SCRIMS_DIR']
 SCRIMS_JSON_PATH = environment['SCRIMS_JSON']
+TEAMS_JSON_PATH = environment['SCRIMS_TEAMS']
+main_team_id = 2586976
+main_team_name = "OG"
 
-scrims_dir = pathlib.Path(SCRIMS_PATH)
-scrims = list(scrims_dir.glob('*.dem'))
+with open(TEAMS_JSON_PATH, 'r') as f:
+    team_map = json.load(f)
 
-# with open('scrims.txt', 'a') as f:
-#     for s in scrims:
-#         print(f"Adding {s.stem}.")
-#         f.write(f"{s.stem}\n")
+team_engine = InitTeamDB()
+team_maker = sessionmaker(bind=team_engine)
+team_session = team_maker()
+unknown_teams = set()
 
+
+def get_team_id(name: Str) -> int:
+    if name in team_map:
+        name = team_map[name]
+    team: TeamInfo = team_session.query(TeamInfo).filter(TeamInfo.name == name).one_or_none()
+    if team is None:
+        unknown_teams.add(name)
+        return None
+    else:
+        return team.team_id
 
 gc = pygsheets.authorize(outh_file="client_secret_1032893103395-vtiha2lmocsru6nvc96ipnrjj10lue23.apps.googleusercontent.com.json")
-# Test sheet
-# sheet = gc.open_by_key('1vZOk0Gx9Wzf45zxnNP8FmOBiXTsYXdyJ7xXE_V60A2o')
-# Real sheet
 sheet = gc.open_by_key('1HbH7GShRz7-kuVVlMaehmSm_AXBSCvGwmYuDYvLfhpA')
 
 scrim_sheet = sheet.worksheet_by_title(r"Past Scrim ID's")
@@ -39,9 +51,26 @@ for scrim_id, name in zip(scrim_ids[2:], team_names[2:]):
     if len(scrim_id) != 10:
         print(f"Skipping unusual scrim ID {scrim_id}")
         continue
-    scrim_dict[int(scrim_id)] = name
+    # Default to the string if its wrong
+    name = name.strip()
+    team_id = get_team_id(name)
+    team_id = team_id if team_id is not None else name
+
+    if team_id in scrim_dict:
+        scrim_dict[team_id][int(scrim_id)] = main_team_name
+    else:
+        scrim_dict[team_id] = {int(scrim_id): main_team_name}
+    # Do OG also
+    if main_team_id in scrim_dict:
+        scrim_dict[main_team_id][int(scrim_id)] = name
+    else:
+        scrim_dict[main_team_id] = {int(scrim_id): name}
 
 
 with open(SCRIMS_JSON_PATH, 'w') as f:
     json.dump(scrim_dict, f)
 
+
+if len(unknown_teams) != 0:
+    print("None matched teams: ")
+    print(', '.join(unknown_teams))
