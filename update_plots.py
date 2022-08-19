cc4944476414b59c6501093f151f0e99a3b99750
Matthew Chadwick <dr.m.chadwick@gmail.com>
2022-08-19 16:42:04 +0100

    Revert again but nicer!

--- update_plots.py
+++ update_plots.py
@@ -284,20 +284,8 @@ def do_draft(team: TeamInfo, metadata,
     (team_path / 'dire').mkdir(parents=True, exist_ok=True)
     (team_path / 'radiant').mkdir(parents=True, exist_ok=True)
 
-    def _save_store(drafts: list, file_stem: str):
-        outputs = []
-        for count, d in enumerate(drafts):
-            output = team_path / f"{file_stem}_{count}.png"
-
-            # d = d.convert("RGB")
-            d.save(output, dpi=(50, 50), optimize=True)
-
-            relpath = str(output.relative_to(Path(PLOT_BASE_PATH)))
-            outputs.append(relpath)
-
-        return outputs
-
     if update_dire:
+        output = team_path / 'dire/drafts.png'
         if per_side_limit is not None:
             replays = r_drafted.filter(dire_filter).order_by(Replay.replayID.desc())\
                                .limit(2*per_side_limit).all()
@@ -308,9 +296,13 @@ def do_draft(team: TeamInfo, metadata,
                                          team,
                                          team.name)
         if dire_drafts is not None:
-            metadata['plot_dire_drafts'] = _save_store(dire_drafts, 'dire/drafts')
+            dire_drafts = dire_drafts.convert("RGB")
+            dire_drafts.save(output, dpi=(50, 50), optimize=True)
+            relpath = str(output.relative_to(Path(PLOT_BASE_PATH)))
+            metadata['plot_dire_drafts'] = relpath
 
     if update_radiant:
+        output = team_path / 'radiant/drafts.png'
         if per_side_limit is not None:
             replays = r_drafted.filter(radiant_filter).order_by(Replay.replayID.desc())\
                                .limit(2*per_side_limit).all()
@@ -321,7 +313,10 @@ def do_draft(team: TeamInfo, metadata,
                                             team,
                                             team.name)
         if radiant_drafts is not None:
-            metadata['plot_radiant_drafts'] = _save_store(radiant_drafts, 'radiant/drafts')
+            radiant_drafts = radiant_drafts.convert("RGB")
+            radiant_drafts.save(output, dpi=(50, 50), optimize=True)
+            relpath = str(output.relative_to(Path(PLOT_BASE_PATH)))
+            metadata['plot_radiant_drafts'] = relpath
 
     if update_radiant or update_dire:
         if per_side_limit is not None:
@@ -331,25 +326,37 @@ def do_draft(team: TeamInfo, metadata,
             replays = r_drafted.order_by(Replay.replayID.desc())\
                                .all()
 
+        output_first = team_path / 'drafts_first.png'
         drafts_first = replay_draft_image(replays,
                                           team,
                                           team.name,
                                           second_pick=False)
         if drafts_first is not None:
-            metadata['plot_drafts_first'] = _save_store(drafts_first, 'drafts_first')
-
+            drafts_first = drafts_first.convert("RGB")
+            drafts_first.save(output_first, dpi=(50, 50), optimize=True)
+            relpath = str(output_first.relative_to(Path(PLOT_BASE_PATH)))
+            metadata['plot_drafts_first'] = relpath
+        
+        output_second = team_path / 'drafts_second.png'
         drafts_second = replay_draft_image(replays,
                                            team,
                                            team.name,
                                            first_pick=False)
         if drafts_second is not None:
-            metadata['plot_drafts_second'] = _save_store(drafts_second, 'drafts_second')
+            drafts_second = drafts_second.convert("RGB")
+            drafts_second.save(output_second, dpi=(50, 50), optimize=True)
+            relpath = str(output_second.relative_to(Path(PLOT_BASE_PATH)))
+            metadata['plot_drafts_second'] = relpath
 
+        output_all = team_path / 'drafts_all.png'
         drafts_all = replay_draft_image(replays,
                                         team,
                                         team.name,)
         if drafts_all is not None:
-            metadata['plot_drafts_all'] = _save_store(drafts_all, 'drafts_all')
+            drafts_all = drafts_all.convert("RGB")
+            drafts_all.save(output_all, dpi=(50, 50), optimize=True)
+            relpath = str(output_all.relative_to(Path(PLOT_BASE_PATH)))
+            metadata['plot_drafts_all'] = relpath
 
     return metadata
 
