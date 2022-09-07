a5e19d907fa96a90cd7d65b908d7c359970f4411
Matthew Chadwick <dr.m.chadwick@gmail.com>
2022-09-07 23:59:43 +0100

    No longer complain about missing ward tables.

--- analysis/route_vis.py
+++ analysis/route_vis.py
@@ -112,7 +112,7 @@ def plot_pregame_players(replay: Replay, team: TeamInfo, side: Team,
         w = wards.filter(Ward.ward_type == w_type)
         data = build_ward_table(w, session, team_session, team)
         if data.empty and w_type == WardType.OBSERVER:
-            print(f"Ward table for {w_type} empty!")
+            # print(f"Ward table for {w_type} empty!")
             continue
         w_icon = w_icons[w_type]
         w_icon.thumbnail(ward_size)
