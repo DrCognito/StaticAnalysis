b9d9dcdb1903b09f8aac2bb402b5e44a72ccb379
Matthew Chadwick <dr.m.chadwick@gmail.com>
2023-04-06 21:13:27 +0100

    Added function to clear the cache of an old value. Added function cache a dataframe row-wise.

--- cache/player_pos.py
+++ cache/player_pos.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine, Column, Integer, BigInteger, Float
 from typing import List
 from pandas import DataFrame, read_sql
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
 
 
 class PlayerPosIndex(CacheItem, Base):
@@ -37,7 +38,7 @@ def is_cached(session: Session, replay_id: int, steam_id: int, version: int) -> 
         return False
 
 
-def get_dataframe(session: Session, replay_ids: List[int],
+def get_cache_dataframe(session: Session, replay_ids: List[int],
                   steam_id: int) -> DataFrame:
     query = session.query(PlayerPos)\
                    .filter(PlayerPos.replayID.in_(replay_ids),
@@ -46,3 +47,48 @@ def get_dataframe(session: Session, replay_ids: List[int],
                                     PlayerPos.yCoordinate,
                                     PlayerPos.count).statement
     return read_sql(sql_query, session.bind)
+
+
+def delete_pos_cache(session: Session, replay_id: int, steam_id: int):
+    try:
+        query = session.query(PlayerPos.steamID == steam_id, PlayerPos.replayID == replay_id)
+        query.delete()
+
+        query = session.query(PlayerPosIndex.steamID == steam_id, PlayerPosIndex.replayID == replay_id)
+        query.delete()
+
+        session.commit()
+    except SQLAlchemyError as e:
+        print(e)
+        print(f"Failed to delete cache for {steam_id} in {replay_id}")
+        session.rollback()
+        exit(2)
+
+
+def add_cache(session: Session, df: DataFrame, replay_id: int, steam_id: int, version: int):
+    # Clear first in case we have an old version.
+    delete_pos_cache(session, replay_id, steam_id)
+    try:
+        for _, row in df.iterrows():
+            new_pos = PlayerPos()
+            new_pos.replayID = replay_id
+            new_pos.steamID = steam_id
+            new_pos.xCoordinate = row['xCoordinate']
+            new_pos.yCoordinate = row['yCoordinate']
+            new_pos.count = row['count']
+
+            session.add(new_pos)
+
+        new_index = PlayerPosIndex()
+        new_index.replayID = replay_id
+        new_index.steamID = steam_id
+        new_index.process_version = version
+
+        session.add(new_index)
+        session.commit()
+    except SQLAlchemyError as e:
+        print(e)
+        print(f"Failed to cache pos for {steam_id} in {replay_id}")
+        session.rollback()
+        exit(2)
+
