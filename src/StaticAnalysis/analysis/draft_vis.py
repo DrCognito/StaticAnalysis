9d4a3ba7e7fbefc33e408f5692a27ed8f555fb07
Matthew Chadwick <dr.m.chadwick@gmail.com>
2023-08-01 00:38:48 +0100

    Initial work adding league+date to the draft.

--- src/StaticAnalysis/analysis/draft_vis.py
+++ src/StaticAnalysis/analysis/draft_vis.py
@@ -14,6 +14,8 @@ from StaticAnalysis.lib.team_info import TeamInfo
 from StaticAnalysis.replays.Replay import Replay, Team
 from StaticAnalysis.replays.TeamSelections import TeamSelections
 
+from datetime import datetime
+
 scims_json = environment['SCRIMS_JSON']
 try:
     with open(scims_json) as file:
@@ -441,7 +443,7 @@ def process_team_dotabuff(replay: Replay, team: TeamSelections, spacing=5):
 
 
 def pickban_line_image(replay: Replay, team: TeamInfo, spacing=5,
-                       add_team_name=True, caching=True):
+                       add_team_name=True, add_league_date=True, caching=False):
     if caching:
         cache_dir = Path(environment["CACHE"])
         file_name = f"{replay.replayID}_{team.name}.png"
@@ -481,6 +483,7 @@ def pickban_line_image(replay: Replay, team: TeamInfo, spacing=5,
     spacerDraw.line([(0,0),(0,team_line.size[1])], fill='black', width=2*spacing)
 
     # Opposition team name text, size concerns?
+    opp_box_width = 0
     if add_team_name:
         font_size = 17
         height = team_line.size[1] + font_size + 2*spacing
@@ -490,6 +493,8 @@ def pickban_line_image(replay: Replay, team: TeamInfo, spacing=5,
                                (255, 255, 255, 0))
         text_canv = ImageDraw.Draw(text_image)
         first_pick_box_offset = 17
+        (left, top, opp_box_width, right) = text_canv.textbbox(xy=(first_pick_box_offset + spacing, spacing),
+                                                      text=opp_name)
         text_canv.text((first_pick_box_offset + spacing, spacing), text=opp_name,
                        font=font, fill=(0, 0, 0))
         # Faction text
@@ -507,6 +512,7 @@ def pickban_line_image(replay: Replay, team: TeamInfo, spacing=5,
 
     width = team_line.size[0] + spacer.size[0] + opposition_line.size[0]
     out_box = Image.new('RGB', (width, height), (255, 255, 255, 0))
+
     if add_team_name:
         out_box.paste(faction_image, (0, 0))
         out_box.paste(team_line, (0, text_image.size[1]))
@@ -519,6 +525,35 @@ def pickban_line_image(replay: Replay, team: TeamInfo, spacing=5,
 
         out_box.paste(opposition_line,
                     (team_line.size[0] + spacer.size[0], 0))
+
+    if add_league_date:
+        font_size = 12
+        font = ImageFont.truetype('arialbd.ttf', font_size)
+        replay_time: datetime = replay.endTimeUTC
+        date_str = replay_time.strftime(r"%b %d %Y")
+        league_str = str(replay.league_ID)
+        text = date_str + ", " + league_str
+
+        ld_image = Image.new('RGB', (team_line.size[0] - opp_box_width, 2*font_size + 2*spacing + 1),
+                             (255, 255, 255, 0))
+        ld_canv = ImageDraw.Draw(ld_image)
+
+        (left, top, right, bottom) = ld_canv.multiline_textbbox(xy=(0, spacing),
+                                                                text=text,
+                                                                spacing=1,
+                                                                align='right')
+        # ld_image = Image.new('RGB', (right, bottom),
+        #                      (255, 0, 255, 255))
+        # ld_canv = ImageDraw.Draw(ld_image)
+        x_off = ld_image.size[0] - right
+        ld_canv.multiline_text(xy=(0, spacing),
+                               text=text,
+                               spacing=1,
+                               align='right',
+                               fill=(0, 0, 0))
+        ld_image = ld_image.crop((0, 0, right, bottom))
+
+        out_box.paste(ld_image, (out_box.size[0] - ld_image.size[0], 0))
 
     if caching:
         cache_dir = Path(environment["CACHE"])
@@ -563,7 +598,7 @@ def replay_draft_image(replays: List[Replay], team: TeamInfo, team_name: str,
         if not is_first and not second_pick:
             continue
 
-        line = pickban_line_image(replay, team, add_team_name=True, caching=True)
+        line = pickban_line_image(replay, team, add_team_name=True, caching=False)
         if line is None:
             continue
 
