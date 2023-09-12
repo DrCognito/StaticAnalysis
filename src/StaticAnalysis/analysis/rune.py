b201aee55a1cb76438196363cc4debde9e7f4fab
Matthew Chadwick <dr.m.chadwick@gmail.com>
2023-09-12 02:20:53 +0100

    Data flow adjustments. Use plot object position. Use get binning percentile. Axis adjustments for the plots mostly working. Change handling of missing team id as it is apparently possible! Manually change array to numpy array (consider process adjust). Remove ticks on quiver plots. Remove some extra comments unused.

--- src/StaticAnalysis/analysis/rune.py
+++ src/StaticAnalysis/analysis/rune.py
@@ -1,7 +1,7 @@
-from StaticAnalysis import Session
+from StaticAnalysis import session, team_session
 from StaticAnalysis.replays.Replay import Replay
 from StaticAnalysis.replays.Player import PlayerStatus
-from StaticAnalysis.analysis.visualisation import plot_player_positioning
+from StaticAnalysis.analysis.visualisation import plot_object_position, get_binning_percentile_xy
 from StaticAnalysis.analysis.route_vis import plot_player_paths
 from pandas import DataFrame
 from StaticAnalysis.lib.team_info import TeamInfo
@@ -10,6 +10,8 @@ import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from StaticAnalysis.replays.Common import Team
 from StaticAnalysis.lib.Common import add_map, EXTENT
+import numpy as np
+
 
 def plot_player_positions(table: DataFrame, main_team: TeamInfo, fig: Figure):
     players = [p.player_id for p in main_team.players]
@@ -20,16 +22,33 @@ def plot_player_positions(table: DataFrame, main_team: TeamInfo, fig: Figure):
     axes[0][1].set_title("Radiant")
 
     for p, n, a in zip(players, names, axes):
-        plot_player_positioning(
-            table[(table['steamID'] == p) & (table['team'] == Team.DIRE)],
-            a[0]
+        dire_df = table.loc[(table['steamID'] == p) & (table['team'] == Team.DIRE)]
+        vmin, vmax = get_binning_percentile_xy(dire_df)
+        vmin = max(1.0, vmin)
+        plot_object_position(
+            dire_df,
+            fig_in=fig,
+            ax_in=a[0],
+            vmin=vmin, vmax=vmax
         )
+        a[0].axis('on')
+        a[0].set_xticks([])
+        a[0].set_yticks([])
         a[0].set_ylabel(n)
-        plot_player_positioning(
-            table[(table['steamID'] == p) & (table['team'] == Team.RADIANT)],
-            a[1]
+
+        radiant_df = table.loc[(table['steamID'] == p) & (table['team'] == Team.RADIANT)]
+        vmin, vmax = get_binning_percentile_xy(radiant_df)
+        vmin = max(1.0, vmin)
+        plot_object_position(
+            radiant_df,
+            fig_in=fig,
+            ax_in=a[1],
+            vmin=vmin, vmax=vmax
         )
-        a[1].ax_in.yaxis.set_label_position("right")
+        a[1].axis('on')
+        a[1].set_xticks([])
+        a[1].set_yticks([])
+        a[1].yaxis.set_label_position("right")
         a[1].set_ylabel(n)
 
     return axes
@@ -44,16 +63,18 @@ def plot_player_routes(table: DataFrame, main_team: TeamInfo, fig: Figure):
                                                      'team': 'first'})
     dire_positions = []
     radiant_positions = []
-    assert main_team.team_id in table['team_id'].unique()
-    for p, t_id, c in zip(table.index, colours):
-        player = get_player_simple(p)
+    if main_team.team_id not in table['team_id'].unique():
+        print(f"Missing main team in {table['replayID'].iloc[0]}")
+
+    for (p, t_id), c in zip(grp.index, colours):
+        player = get_player_simple(p, team_session)
         if player:
             name = player.name
         else:
             name = p
-        x = grp.xs(p, t_id)['xCoordinate']
-        y = grp.xs(p, t_id)['yCoordinate']
-        team = grp.xs(p, t_id)['team']
+        x = np.array(grp.xs((p, t_id))['xCoordinate'])
+        y = np.array(grp.xs((p, t_id))['yCoordinate'])
+        team = grp.xs((p, t_id))['team']
         plot = axis.quiver(x[:-1], y[:-1], x[1:] - x[:-1], y[1:] - y[:-1],
                            scale_units='xy', angles='xy', scale=1,
                            zorder=2, color=c, label=name)
@@ -64,6 +85,8 @@ def plot_player_routes(table: DataFrame, main_team: TeamInfo, fig: Figure):
     xMin, xMax, yMin, yMax = EXTENT
     axis.set_xlim(xMin, xMax)
     axis.set_ylim(yMin, yMax)
+    axis.set_xticks([])
+    axis.set_yticks([])
 
     # Create a legend for the first line.
     first_legend = axis.legend(handles=dire_positions, loc='upper right')
@@ -73,25 +96,3 @@ def plot_player_routes(table: DataFrame, main_team: TeamInfo, fig: Figure):
     axis.legend(handles=radiant_positions, loc='lower left')
 
     return axis
-
-
-# def plot_player_paths(paths, colours, names, axis):
-#     assert(len(paths) <= len(colours))
-#     # add_map(axis)
-#     plots = []
-#     for colour, path, name in zip(colours, paths, names):
-#         if path.empty:
-#             continue
-#         x = path['xCoordinate'].to_numpy()
-#         y = path['yCoordinate'].to_numpy()
-
-#         plot = axis.quiver(x[:-1], y[:-1], x[1:]-x[:-1], y[1:]-y[:-1],
-#                            scale_units='xy', angles='xy', scale=1,
-#                            zorder=2, color=colour, label=name)
-#         plots.append(plot)
-#         axis.axis('off')
-#     xMin, xMax, yMin, yMax = EXTENT
-#     axis.set_xlim(xMin, xMax)
-#     axis.set_ylim(yMin, yMax)
-
-#     return plots
