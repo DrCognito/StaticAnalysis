ea2cd16c0ff6e8bdc49305fbdf8e8bbba3a9ace3
Matthew Chadwick <dr.m.chadwick@gmail.com>
2024-05-23 15:31:09 +0100

    Remade to check and test new processes

--- src/StaticAnalysis/snippets/rune_routes.py
+++ src/StaticAnalysis/snippets/rune_routes.py
@@ -2,7 +2,7 @@ from StaticAnalysis import session, team_session
 from StaticAnalysis.replays.Replay import Replay
 from StaticAnalysis.replays.Player import PlayerStatus, Player
 from StaticAnalysis.replays.Rune import Rune, RuneID
-from herotools.important_times import MAIN_TIME
+from herotools.important_times import MAIN_TIME, ImportantTimes
 from StaticAnalysis.lib.team_info import TeamInfo, TeamPlayer
 from sqlalchemy import and_, or_
 from pandas import read_sql
@@ -10,6 +10,8 @@ import numpy as np
 from StaticAnalysis.replays.Common import Team
 from StaticAnalysis.analysis.visualisation import plot_player_positioning
 from StaticAnalysis.analysis.route_vis import plot_player_paths
+from StaticAnalysis.analysis.Player import player_position_replays, player_positioning_single
+from StaticAnalysis.analysis.rune import wisdom_rune_times
 
 
 def get_team(name) -> TeamInfo:
@@ -19,64 +21,106 @@ def get_team(name) -> TeamInfo:
     return team
 
 team = get_team(2586976)
-r_query = team.get_replays(session).filter(Replay.endTimeUTC >= MAIN_TIME)
+r_query = team.get_replays(session).filter(Replay.endTimeUTC >= ImportantTimes['Birmingham'])
 
-t_filter = (PlayerStatus.game_time > 7 * 60 - 30,
-            PlayerStatus.game_time <= 7 * 60 + 10)
-p_query = (session.query(PlayerStatus.xCoordinate, PlayerStatus.yCoordinate,
-                         PlayerStatus.team_id, PlayerStatus.steamID, PlayerStatus.team, PlayerStatus.replayID)
-                  .join(r_query.subquery()).filter(*t_filter)
-)
-t_query = (session.query(PlayerStatus)
-                  .join(r_query.subquery()).filter(*t_filter)
+start = 6.5 * 60
+rune_times = wisdom_rune_times(r_query, max_time=13 * 60)
+end = int(rune_times['game_time'].max())
+# end = 550
+test_query = player_position_replays(
+    session, r_query, start=start, end=end
 )
 
-p_pos = read_sql(p_query.statement, session.bind)
-# lanes = pos_df.groupby(by=['team', 'hero',])['lane'].apply(lambda x: x.value_counts().index[0])
-r_ids = {x.replayID for x in r_query}
-p2_query = (
-    session.query(PlayerStatus.xCoordinate, PlayerStatus.yCoordinate,
-                  PlayerStatus.team_id, PlayerStatus.steamID)
-                 .filter(PlayerStatus.replayID.in_(r_ids))
-                 .filter(*t_filter)
+filter = (PlayerStatus.game_time > start,
+              PlayerStatus.game_time <= 550)
+
+query = (
+    session.query(
+        PlayerStatus.xCoordinate, PlayerStatus.yCoordinate, PlayerStatus.team_id,
+        PlayerStatus.steamID, PlayerStatus.replayID, PlayerStatus.team, PlayerStatus.game_time
+        )
+        .filter(*filter)
+        .join(r_query.subquery())
 )
 
-tesT_id = 7287618636
-t2_query = (session.query(PlayerStatus)
-                  .filter(PlayerStatus.replayID == tesT_id).filter(*t_filter)
-)
-p2_pos = read_sql(t2_query.statement, session.bind)
-print(p2_pos.team_id.unique())
+def player_position_single(
+    session, r_query,
+    team: TeamInfo, player: TeamPlayer,
+    side: Team,
+    start: int, end: int, recent_limit=None):
 
-from sqlalchemy import select
-from StaticAnalysis.replays.TeamSelections import TeamSelections
-blah = ( 
-        select([TeamSelections.teamID])
-        .where(TeamSelections.replay_ID == 7287618636)
-        .where(TeamSelections.team == Team.RADIANT)
-)
-p_grp = p_pos.groupby(['steamID', 'team_id']).agg(list)
-p_grp.xs(2586976, level=1)
+    t_filter = ()
+    if start is not None:
+        t_filter += (PlayerStatus.game_time >= start,)
+    if end is not None:
+        t_filter += (PlayerStatus.game_time <= end,)
 
-t_player = team_session.query(TeamPlayer).filter(TeamPlayer.player_id == 76561198053884305)
+    steam_id = player.player_id
 
-import timeit
-# Get player ids manually
-start_time = timeit.default_timer()
-player_query = session.query(Player).filter(Player.replayID == 7287618636)
-players_queries = [(p.steamID, p.team) for p in player_query]
-print(timeit.default_timer() - start_time)
+    r_filter = Replay.get_side_filter(team, side)
+    replays = r_query.filter(r_filter).subquery()
+    if recent_limit is not None:
+        replays = r_query.filter(r_filter).order_by(Replay.replayID.desc()).limit(recent_limit).subquery()
 
-# Get player ids from our table
-filtered_tab = p_pos.loc[p_pos['replayID'] == 7287618636]
-start_time = timeit.default_timer()
-#.groupby(['steamID']).agg(min)
-players_filt = filtered_tab.loc[:, ["steamID", "team"]].drop_duplicates()
-ft_players = list(players_filt.itertuples(index=False, name=None))
-print(timeit.default_timer() - start_time)
+    p_filter = t_filter + (PlayerStatus.steamID == steam_id,
+                            PlayerStatus.team == side)
 
-rune_query = (    
-    session.query(Rune.game_time, Rune.replayID)
-                  .join(r_query.subquery())
-                  .filter(Rune.runeType == RuneID.Wisdom, Rune.game_time < 13 * 60)
-)
+    player_q = session.query(PlayerStatus)\
+                        .filter(*p_filter)\
+                        .join(replays)
+
+    return player_q
+
+p_query = player_position_single(
+            session, r_query,
+            team, team.players[0],
+            Team.DIRE,
+            start, end=550, recent_limit=5
+        )
+from pandas import DataFrame
+def dataframe_xy(query, session) -> DataFrame:
+    sql_query = query.with_entities(
+        PlayerStatus.xCoordinate, PlayerStatus.yCoordinate,
+        PlayerStatus.team_id, PlayerStatus.steamID, PlayerStatus.replayID,
+        PlayerStatus.team, PlayerStatus.game_time).statement
+
+    data = read_sql(sql_query, session.bind)
+
+    return data
+
+# table = dataframe_xy(p_query, session)
+
+# from pandas import read_sql
+# table2 = read_sql(query.statement, session.bind)
+
+def player_position_replay_id(session, replay_id: int, start: int, end: int) -> DataFrame:
+    '''
+    More general player position table.
+    start: int in seconds
+    end: int in seconds
+    '''
+    filter = (
+        PlayerStatus.replayID == replay_id,
+        PlayerStatus.game_time > start,
+        PlayerStatus.game_time <= end
+        )
+
+    query = (
+        session.query(PlayerStatus.xCoordinate, PlayerStatus.yCoordinate, PlayerStatus.team_id,
+                      PlayerStatus.steamID, PlayerStatus.replayID, PlayerStatus.team, PlayerStatus.game_time)
+               .filter(*filter)
+            #    .join(r_query.subquery())
+    )
+
+    pos_table = read_sql(query.statement, session.bind)
+
+    return pos_table
+
+data = []
+r: Replay
+for r in r_query:
+    rune_max = rune_times[rune_times['replayID'] == r.replayID]['game_time'].max()
+    rune_max = int(rune_max)
+    data.append(player_position_replay_id(session, r.replayID,
+                                          start=start, end=rune_max))
+
