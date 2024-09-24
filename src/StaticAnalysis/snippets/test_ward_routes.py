c876cf7b33fe7a45d8e974cadf032dae4d8fb246
Matthew Chadwick <dr.m.chadwick@gmail.com>
2024-09-24 13:54:04 +0100

    update for a more recent replay

--- src/StaticAnalysis/snippets/test_ward_routes.py
+++ src/StaticAnalysis/snippets/test_ward_routes.py
@@ -1,20 +1,20 @@
-from StaticAnalysis.tests.minimal_db import session, team_session, TeamInfo
+from StaticAnalysis.snippets.minimal_db import session, team_session, TeamInfo
 from StaticAnalysis.replays.Replay import Replay, Team
 from StaticAnalysis.analysis.route_vis import plot_pregame_players
 import matplotlib.pyplot as plt
 
-replay = session.query(Replay).filter(Replay.replayID == 7117458330).one()
-team_alliance = team_session.query(TeamInfo).filter(TeamInfo.team_id == 111474).one()
-team_att = team_session.query(TeamInfo).filter(TeamInfo.team_id == 8687717).one()
+replay = session.query(Replay).filter(Replay.replayID == 7957516700).one()
+nigma = team_session.query(TeamInfo).filter(TeamInfo.team_id == 7554697).one()
+avulus = team_session.query(TeamInfo).filter(TeamInfo.team_id == 9498970).one()
 
 fig = plt.figure(figsize=(7, 7))
 
-plot_pregame_players(replay, team_alliance, Team.DIRE, session, team_session, fig)
+plot_pregame_players(replay, nigma, Team.DIRE, session, team_session, fig)
 fig.tight_layout()
-fig.savefig("test_alliance_dire.png")
+fig.savefig("wardroute_dire.png")
 fig.clf()
 
-plot_pregame_players(replay, team_att, Team.RADIANT, session, team_session, fig)
+plot_pregame_players(replay, avulus, Team.RADIANT, session, team_session, fig)
 fig.tight_layout()
-fig.savefig("test_att_radiant.png")
+fig.savefig("wardroute_radiant.png")
 fig.clf()
