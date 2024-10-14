fc32397af93bae9d8ae40ae0556b0152f61ce2bb
Matthew Chadwick <dr.m.chadwick@gmail.com>
2024-10-14 16:59:45 +0100

    Swap to setup.toml Lib cleanup

--- src/StaticAnalysis/__init__.py
+++ src/StaticAnalysis/__init__.py
@@ -1,13 +1,14 @@
-from dotenv import load_dotenv
 from sqlalchemy.orm import sessionmaker
 from propubs.model.pub_heroes import InitDB as InitPubDB
-from StaticAnalysis.lib.team_info import InitTeamDB, TeamInfo
-from StaticAnalysis.replays.Replay import InitDB, Replay, Team
-from os import environ as environment
+from StaticAnalysis.lib.team_info import InitTeamDB
+from StaticAnalysis.replays.Replay import InitDB
+import tomllib
 
-load_dotenv(dotenv_path="../../setup.env")
-DB_PATH = environment['PARSED_DB_PATH']
-PLOT_BASE_PATH = environment['PLOT_OUTPUT']
+with open('../../setup.toml', 'rb') as f:
+    CONFIG = tomllib.load(f)
+
+DB_PATH = CONFIG['database']['PARSED_DB_PATH']
+PLOT_BASE_PATH = CONFIG['output']['PLOT_OUTPUT']
 
 engine = InitDB(DB_PATH)
 Session = sessionmaker(bind=engine)
