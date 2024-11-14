535d368773761a28c1063331ea5b6e4b19a10b4d
Matthew Chadwick <dr.m.chadwick@gmail.com>
2024-11-14 18:59:45 +0000

    Gladiators checking for more busy smokes

--- src/StaticAnalysis/snippets/test_ward_routes.py
+++ src/StaticAnalysis/snippets/test_ward_routes.py
@@ -1,7 +1,7 @@
 from StaticAnalysis import session, team_session
 from StaticAnalysis.lib.team_info import TeamInfo
 from StaticAnalysis.replays.Replay import Replay, Team
-from StaticAnalysis.analysis.route_vis import plot_pregame_players
+from StaticAnalysis.analysis.route_vis import plot_pregame_players, get_player_dataframes
 from StaticAnalysis.analysis.smoke_vis import smoke_start_locale, smoke_end_locale_first, smoke_end_locale_individual, get_smoke_time_info
 import matplotlib.pyplot as plt
 from StaticAnalysis.replays.Player import Player, PlayerStatus
@@ -12,43 +12,29 @@ replay = session.query(Replay).filter(Replay.replayID == 7957516700).one()
 nigma = team_session.query(TeamInfo).filter(TeamInfo.team_id == 7554697).one()
 avulus = team_session.query(TeamInfo).filter(TeamInfo.team_id == 9498970).one()
 
+glad_test1 = session.query(Replay).filter(Replay.replayID == 8011119646).one()
+glad_test2 = session.query(Replay).filter(Replay.replayID == 8009585260).one()
+gladiators = team_session.query(TeamInfo).filter(TeamInfo.team_id == 8599101).one()
+
 fig = plt.figure(figsize=(7, 7))
 
-plot_pregame_players(replay, nigma, Team.DIRE, session, team_session, fig)
+plot_pregame_players(glad_test1, gladiators, Team.RADIANT, session, team_session, fig)
 fig.tight_layout()
-fig.savefig("wardroute_dire.png")
+fig.savefig("wardroute_glad1.png")
 fig.clf()
 
-plot_pregame_players(replay, avulus, Team.RADIANT, session, team_session, fig)
+plot_pregame_players(glad_test2, gladiators, Team.RADIANT, session, team_session, fig)
 fig.tight_layout()
-fig.savefig("wardroute_radiant.png")
+fig.savefig("wardroute_glad2.png")
 fig.clf()
 
-side = Team.DIRE
-dire_player_loc = []
-for player in replay.players:
-    if player.team != side:
-        continue
-    query = player.status.filter(PlayerStatus.game_time <= 45)
-    # query = player.status.filter(PlayerStatus)
+side = Team.RADIANT
+glad_df1 = get_player_dataframes(glad_test1, side, session)
+glad_df2 = get_player_dataframes(glad_test2, side, session)
 
-    sql_query = query.with_entities(
-        PlayerStatus.xCoordinate,
-        PlayerStatus.yCoordinate,
-        PlayerStatus.is_smoked,
-        PlayerStatus.game_time,
-        PlayerStatus.is_alive).statement
+print(f"Smoke start: {smoke_start_locale(glad_df2)}")
+print(f"Smoke end (by first break): {smoke_end_locale_first(glad_df2)}")
+print(f"Smoke end (by everyones break): {smoke_end_locale_individual(glad_df2)}")
+print(get_smoke_time_info(glad_df2))
 
-    p_df = read_sql(sql_query, session.bind)
-    p_df['location'] = p_df.apply(
-        lambda x: get_player_location(x['xCoordinate'], x['yCoordinate']),
-        axis=1
-        )
-    dire_player_loc.append(p_df)
-
-print(f"Smoke start: {smoke_start_locale(dire_player_loc)}")
-print(f"Smoke end (by first break): {smoke_end_locale_first(dire_player_loc)}")
-print(f"Smoke end (by everyones break): {smoke_end_locale_individual(dire_player_loc)}")
-print(get_smoke_time_info(dire_player_loc))
-
-test_table = get_smoke_time_info(dire_player_loc)
+test_table = get_smoke_time_info(glad_df2)
