cf28b273db256d1da2d9db7a1549a93a559965c3
Matthew Chadwick <dr.m.chadwick@gmail.com>
2025-04-15 01:33:13 +0100

    Added a custom class to help memoize and cache fonts. Added global font cache.

--- src/StaticAnalysis/__init__.py
+++ src/StaticAnalysis/__init__.py
@@ -4,6 +4,8 @@ from StaticAnalysis.lib.team_info import InitTeamDB
 from StaticAnalysis.replays.Replay import InitDB
 from pathlib import Path
 import tomllib
+from collections import defaultdict
+from PIL import ImageFont
 
 config_path = Path('setup.toml')
 try:
@@ -28,3 +30,17 @@ team_session = team_maker()
 pub_engine = InitPubDB()
 pub_maker = sessionmaker(bind=pub_engine)
 pub_session = pub_maker()
+
+# https://stackoverflow.com/questions/71689286/any-workaround-to-pass-multiple-arguments-to-defaultdicts-default-factory
+class CacheDict(dict):
+    def __init__(self, factory: callable):
+        self.factory = factory
+    def __missing__(self, key):
+        if self.factory is None:
+            raise KeyError(key)
+        
+        value = self.factory(key)
+        self[key] = value
+        return value
+
+FONT_CACHE = CacheDict(lambda x: ImageFont.truetype(x[0], x[1]))
