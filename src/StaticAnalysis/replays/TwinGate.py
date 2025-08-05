7b4868ae8dd753b27a82677297bd271e51995b5c
Matthew Chadwick <dr.m.chadwick@gmail.com>
2025-08-06 00:21:20 +0100

    Changed how completed works as pros cant help themselves

--- src/StaticAnalysis/replays/TwinGate.py
+++ src/StaticAnalysis/replays/TwinGate.py
@@ -2,7 +2,8 @@ from sqlalchemy import (
     BigInteger, Column, ForeignKey, Integer, String, 
     Boolean, create_engine)
 from sqlalchemy.types import Enum
-from StaticAnalysis import LOG
+# from StaticAnalysis import LOG
+from loguru import logger as LOG
 from StaticAnalysis.lib.Common import EXTENT
 from StaticAnalysis.replays.Common import Team
 from StaticAnalysis.replays import Base
@@ -17,12 +18,11 @@ class TwinGate(Base):
         BigInteger, ForeignKey("Replays.replayID"),
         primary_key=True
         )
-    replay = relationship("Replay", back_populates="twin_gate")
+    replay = relationship("Replay", back_populates="twin_gates")
     steamID = Column(BigInteger, primary_key=True)
     channel_start = Column(Integer, primary_key=True)
     channel_end = Column(Integer, primary_key=True)
     hero = Column(String)
-    completes = Column(Boolean)
     side_pos = Column(Enum(Team))
 
     def __init__(self, replay_in):
@@ -42,16 +42,17 @@ def get_gate_pos(yPos: float) -> Team:
     return Team.DIRE if diffMax < diffMin else Team.RADIANT
 
 
-def populate_from_JSON(json, replay_in) -> List[TwinGate]:
+def populate_from_JSON(json, replay_in, session) -> List[TwinGate]:
     summary = get_twin_gates(json)
     gates = []
 
     for s in summary:
+        if not s['completed']:
+            continue
         gate = TwinGate(replay_in)
         gate.hero = s['hero']
         gate.channel_start = s['start'] - replay_in.creepSpawn
         gate.channel_end = s['end'] - replay_in.creepSpawn
-        gate.completes = s['completed']
         gate.side_pos = get_gate_pos(s['startY'])
         # Get the steam ID and channel position for tormie pos
         for rp in replay_in.players:
@@ -61,7 +62,9 @@ def populate_from_JSON(json, replay_in) -> List[TwinGate]:
         if gate.steamID is None:
             LOG.warning(f"Could not asign steamID for {gate.hero} in {gate.replayID} at start {s['start']}")
 
+        session.add(gate)
         gates.append(gate)
+        
 
     return gates
 
