5260e944105340a9986d651512beb681dacf9b3c
Matthew Chadwick <dr.m.chadwick@gmail.com>
2026-06-30 17:21:44 +0100

    Global gates testing

--- src/StaticAnalysis/snippets/global_plots.py
+++ src/StaticAnalysis/snippets/global_plots.py
@@ -204,3 +204,97 @@ names = ["P1", "P2", "P3", "P4", "P5", "Total"]
 # both_plot = plot_stacks(both, names, "Combined", tot_replays)
 # radiant_plot = plot_stacks(radiant, names, "Radiant", tot_replays)
 dire_plot = plot_stacks(dire, names, "Dire", tot_replays)
+
+tg_time_binning = IntervalIndex.from_tuples([
+    (4*60, 8*60), # 4 to 8mins
+    (8*60, 20*60), # 8 to 20mins
+    (20*60, 30*60), # 20 to 30mins
+    (30*60, 40*60), # 30 to 40mins
+    (40*60, 400*60) # 40+
+    ])
+tg_time_names = [
+    "4 to 8mins",
+    "8 to 20mins",
+    "20 to 30mins",
+    "30 to 40mins",
+    ">40mins",
+]
+tg_time_map = {k:v for k,v in zip(tg_time_binning, tg_time_names)}
+
+from StaticAnalysis.replays.TwinGate import TwinGate
+def get_dataframe_counts_global(
+    r_query: Query, session: Session,
+    pos_dict: dict[tuple[int,int], Position] | None = None) -> tuple[DataFrame, int]:
+    '''
+    Builds a set of gate transitions from the set of replays requiring team matching to team in
+    TwinGate so the team should be correct!
+    Returns DataFrame and team specific player game counts.
+    '''
+    # Replay ids for joining
+    rids = [r.replayID for r in r_query.all()]
+    if pos_dict is None:
+        # Get the position dictionary for available replays from r_query dataset
+        qry = select(RolePosition).where(RolePosition.replayID.in_(rids))
+        role_pos = session.execute(qry).all()
+        p: RolePosition
+        pos_dict = {
+            (p[0].replayID, p[0].steamID):p[0].position for p in role_pos
+        }
+    pos_map = {
+        Position.SAFE: "P1",
+        Position.MID: "P2",
+        Position.OFF: "P3",
+        Position.P4: "P4",
+        Position.P5: "P5",
+    }
+    count = 0
+    gate_list = []
+    gate_query = select(TwinGate).where(
+        TwinGate.hero != 'npc_dota_hero_meepo',
+        TwinGate.replayID.in_(rids))
+    for g in session.scalars(gate_query):
+        pos = pos_dict.get((g.replayID, g.steamID))
+        if pos is None:
+            continue
+        gate_list += [
+           {'pos': pos, 'time':g.channel_start, 'gate side':g.side_pos,
+            'name': pos_map[pos], 'replayID': g.replayID}
+        ]
+
+    df = DataFrame(gate_list)
+    count = df['replayID'].nunique() * 2 # For two teams
+    
+    # Setup the DataFrame
+    df['tBin'] = cut(df['time'], tg_time_binning)
+    # Remove any null values outside tbin range
+    df = df[~df['tBin'].isnull()]
+    # Labels wont work on IntervalIndex
+    df['tBin'] = df['tBin'].map(tg_time_map)
+    df['count'] = 1
+    
+    # Pivot table to per time interval representation
+    df = (df[['name', 'tBin', 'count']]
+        .pivot_table(index='tBin', columns='name', aggfunc='sum', observed=True)
+        )
+    # Remove extraneous count multicol
+    df.columns = list(zip(*df.columns))[1]
+    # Some times NaNs can show up for some reason, handle them
+    df = df.fillna(0).astype(int)
+
+    return df, count
+
+gate_df, count = get_dataframe_counts_global(r_query, session)
+
+from StaticAnalysis.vis.twin_gate import plot_twingate_table
+
+fig, axe = plt.subplots(figsize=(8, 4), layout='constrained')
+for col in gate_df.columns:
+    gate_df[col] = gate_df[col] / count
+axe = plot_twingate_table(axe, gate_df, ".2f")
+# axe = add_count(axe, count)
+axe.set_title("Global Dataset Average", pad=20)
+y_font = axe.get_yticklabels()[0].get_fontproperties()
+axe.text(
+    x=0.5*5, y=-0.1, s=f'{count} (team) Games',
+    ha='center', va='baseline',
+    fontproperties=y_font)
